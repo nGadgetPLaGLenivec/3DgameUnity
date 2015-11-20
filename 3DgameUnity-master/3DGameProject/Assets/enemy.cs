3887b2af887cf71ccfaefd7f11eae6e7d52d99f6
psmax <shmarov_max@mail.ru>
2015-11-20 09:00:50 +0300

    change
    Паутина и вражина

---
+++ 3DgameUnity-master/3DGameProject/Assets/enemy.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemy : MonoBehaviour {
+
+    public Transform player;
+    public float move_speed;
+    public float rotation_speed;
+    public Transform enemies;
+    
+    void Update()
+    {
+        var look_dir = player.position - enemies.position;
+        look_dir.y = 0;
+        enemies.rotation = Quaternion.Slerp(enemies.rotation, Quaternion.LookRotation(look_dir), rotation_speed * Time.deltaTime);
+        enemies.position += enemies.forward * move_speed * Time.deltaTime;
+    }
+}
---
+++ 3DgameUnity-master/3DGameProject/Assets/spider.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+
+public class spider : MonoBehaviour {
+    // Use this for initialization
+    bool inWeb = false;
+    UnityStandardAssets.Characters.ThirdPerson.ThirdPersonCharacter slow;
+    void Start () {
+  
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        /*if (!inWeb)
+        {
+            slow.m_MovingTurnSpeed = 360;
+            slow.m_MoveSpeedMultiplier = 1F;
+        }*/
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        slow = GetComponent<UnityStandardAssets.Characters.ThirdPerson.ThirdPersonCharacter>();
+        if (collision.gameObject.name == "web_spider")
+        {
+            inWeb = true;
+       
+            slow.m_MovingTurnSpeed = 0F;
+            slow.m_MoveSpeedMultiplier = 0.3F;
+
+        }
+        else
+        {
+            slow.m_MovingTurnSpeed = 360;
+            slow.m_MoveSpeedMultiplier = 1F;
+        }
+    }
+
+}
