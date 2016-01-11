b40cc35bc012c564413737aa343300ca63b8a46f
psmax <shmarov_max@mail.ru>
2016-01-12 02:35:44 +0300

    Enemy
    Enemy

--- 3DgameUnity-master/3DGameProject/Assets/Scripts/Enemy.cs
+++ 3DgameUnity-master/3DGameProject/Assets/Scripts/Enemy.cs
@@ -1,19 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Enemy : MonoBehaviour {
+public class Enemy : MonoBehaviour
+{
 
-    public Transform player;
+    public GameObject player;
     public float move_speed;
     public float rotation_speed;
     public Transform enemies;
+    public float spawn_time = 3f;
+    public Transform[] spawn_points;
 
 
-void Update()
+    void Start()
     {
-        var look_dir = player.position - enemies.position;
+        // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
+        InvokeRepeating("Spawn", spawn_time, spawn_time);
+    }
+
+    void Update()
+    {
+        var look_dir = player.transform.position - enemies.position;
         look_dir.y = 0;
         enemies.rotation = Quaternion.Slerp(enemies.rotation, Quaternion.LookRotation(look_dir), rotation_speed * Time.deltaTime);
         enemies.position += enemies.forward * move_speed * Time.deltaTime;
+    }
+    void Spawn()
+    {
+        int spawnPointIndex = Random.Range(0, spawn_points.Length);
+        //  Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
+        Instantiate(enemies, spawn_points[spawnPointIndex].position, spawn_points[spawnPointIndex].rotation);
     }
 }
